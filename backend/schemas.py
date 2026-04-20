51ba566d2909b9bc4c371eab5c776ae0dcaec090
Icaru-s <icaro2007ryan@gmail.com>
2026-04-19 21:58:17 -0300

    create: schemas

---
+++ backend/schemas.py
@@ -0,0 +1,8 @@
+# Certifica que as entradas receberão a tipagem certa
+
+from pydantic import BaseModel, EmailStr
+
+# Dados que o usuário envia no cadastro
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str

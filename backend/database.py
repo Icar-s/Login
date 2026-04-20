3c0ed986a16c3e71f77bdf5aa9e5508546df5d52
Icaru-s <icaro2007ryan@gmail.com>
2026-04-20 17:00:39 -0300

    fix: database

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,16 @@
-# Importa ferramentas do SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# URL do banco (SQLite)
-# "sqlite:///./test.db" → cria um arquivo test.db na pasta
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Cria o motor de conexão com o banco
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}  # necessário para o SQLite funcionar com FastAPI
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
-# Cria uma fábrica de sessões (conexões com o banco)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base que vai ser usada pra criar modelos (tabelas)
 Base = declarative_base()
-
